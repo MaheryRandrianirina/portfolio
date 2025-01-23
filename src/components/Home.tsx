1642cf3d44b237f662400e12f0f3d83d343913e3
MaheryRandrianirina <106425085+MaheryRandrianirina@users.noreply.github.com>
2025-01-23 21:16:59 +0300

    feat: add style to home header text. Add also photo

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import homeModule from "../css/modules/home.module.css";
 import { content } from "@/components/_home-content";
+import Image from "next/image";
+
 const navList = [
     "home",
     "about",
@@ -14,14 +16,26 @@ export const Home: FC<{}> = ()=>{
             <p>logo</p>
             <nav>
                 <ul>
-                    {navList.map(menu => <li><a href={`#${menu}`}>{menu}</a></li>)}
+                    {navList.map(menu => <li key={`${menu}_${navList.indexOf(menu)}`}><a href={`#${menu}`}>{menu}</a></li>)}
                 </ul>
             </nav>
         </header>
         <div className={homeModule.body}>
-            <h1 className="title">{content.fr?.title}</h1>
-            <p className="description">{content.fr?.description}</p>
-            <small className="label">{content.fr?.label}</small>
+            <div className={homeModule.left}>
+                <h1 className="title">{content.fr?.title}</h1>
+                <div>
+                    <p className="description">{content.fr?.description}</p>
+                    <small className="label">{content.fr?.label}</small>
+                </div>
+            </div>
+            <div className={homeModule.right}>
+                <Image 
+                    src="/images/photo.jpg" 
+                    alt={content.fr?.photo.alt as string}
+                    width={200} 
+                    height={200}
+                />
+            </div>
         </div>
     </section>
 }
--- src/components/_home-content.ts
+++ src/components/_home-content.ts
@@ -2,16 +2,22 @@ import { Languages } from "@/common/types"
 
 type HomeContent<T extends Languages> = {
     [key in keyof T]?: {
-        "title": string,
-        "description": string,
-        "label": string
+        title: string,
+        description: string,
+        label: string,
+        photo: {
+            alt: string
+        }
     }
 }
 
 export const content: HomeContent<Languages> = {
     fr: {
-        "title": "Développeur web",
-        "description": "Hello, je m'appelle RANDRIANIRINA Miranto Fifaliana Mahery.",
-        "label": "Créons quelque chose de grand ensemble !"
+        title: "Développeur web",
+        description: "Hello, je m'appelle RANDRIANIRINA Miranto Fifaliana Mahery.",
+        label: "Créons quelque chose de grand ensemble !",
+        photo: {
+            alt: "Photo du super développeur relévant des défis jouraprès jour"
+        }
     }
 }
